50ea6a981dc4911c30e0f850d46ea7db3b43f826
Rahul Sharam <rahul.sharma@core-solutions.in>
2020-07-04 04:07:00 +0530

    added age turned, days left in birthday api

--- app/Birthday.php
+++ app/Birthday.php
@@ -10,6 +10,8 @@ class Birthday extends Model
 {
     protected $fillable = ['image','first_name','last_name','birthday','email','mobile','note','created_by'];
 
+    protected $appends = ['birth_date','days_left_or_before','turned_age'];
+
     public function getImageAttribute($value){
         return url(route('public-image',['disk'=>'birthday','image'=>$value]));
     }
@@ -19,7 +21,22 @@ class Birthday extends Model
     }
 
     public function getBirthdayAttribute($value){
-        $this->birth_date = $value;
+        $this->attributes['birth_date'] = $value;
         return Carbon::parse($value)->format('M').' '.Carbon::today()->format('Y');
     }
+
+    public function getBirthDateAttribute($value){
+        return $this->attributes['birth_date'];
+    }
+
+    public function getDaysLeftOrBeforeAttribute($value){
+        $dob = Carbon::createFromFormat('Y-m-d',$this->attributes['birth_date'])->format('m-d');
+        $dob = Carbon::createFromFormat('m-d',$dob)->format('Y-m-d');
+        return Carbon::parse($dob)->diff(Carbon::today())->days;
+    }
+
+    public function getTurnedAgeAttribute(){
+        return Carbon::today()->diff(Carbon::parse($this->attributes['birth_date']))->y;
+    }
+
 }
