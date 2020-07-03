0b943ee47a77b03d6e0bd7496ec7f24ede810aa3
Rahul Sharam <rahul.sharma@core-solutions.in>
2020-07-03 23:52:49 +0530

    add label counts key in get labels response

--- app/Label.php
+++ app/Label.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Label extends Model
 {
     protected $fillable = ['label_name','label_color','created_by'];
+
+    protected $appends = ['total_counts'];
+
+    public function label_count(){
+        return $this->hasMany(LabelMapping::class,'label_id','id');
+    }
+
+    public function getTotalCountsAttribute(){
+        return $this->hasMany(LabelMapping::class)->whereUserId(Auth::user()->id)->count();
+    }
 }
